12ad6b425de77054e67c9e65550983dd0f72a027
Swathi P <swathisoman2353@gmail.com>
2023-06-07 16:38:47 +0530

    update

--- administrator/views.py
+++ administrator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from core.models import Product
 
 # Create your views here.
 
@@ -7,4 +8,8 @@ def dashboard(request):
 def about_admin(request):
     return render(request,'admin/about_admin.html' )
 def products(request):
-    return render(request,'admin/products.html')
+    products = Product.objects.filter(status=1)
+    context = {
+         'products' : products,
+    }
+    return render(request,'admin/products.html',context)
